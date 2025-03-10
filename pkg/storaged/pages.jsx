a68c4a1710137e39de778ef82b87588bbecc0195
Jelle van der Waa <jvanderwaa@redhat.com>
2025-03-10 13:18:28 +0100

    dbg: do not merge

--- pkg/storaged/pages.jsx
+++ pkg/storaged/pages.jsx
@@ -15,6 +15,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Cockpit; If not, see <https://www.gnu.org/licenses/>.
+ *
+ * WOOPS
  */
 
 import cockpit from "cockpit";
